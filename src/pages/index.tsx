c6328ca5495aef5f8add08bf31f38e71d724e065
Hunter Jennings <jenningsdhunter@gmail.com>
2020-12-31 08:36:57 -0500

    moving writings list to writings page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,10 @@
-import Link from 'next/link';
-import fs from 'fs';
-import matter from 'gray-matter';
-import path from 'path';
 import { MainLayout } from '@/components/layouts/MainLayout/MainLayout';
 import { NextSeo } from 'next-seo';
 
-import { writingsFilePaths, WRITINGS_PATH } from '../utils/mdxUtils';
-
 import styles from '@scss/pages/Home.module.scss';
-import { GetStaticProps } from 'next';
 import { PageWithLayoutType } from '@/components/layouts/layouts.model';
-import { CustomLink } from '@/components/CustomLink/CustomLink';
-import { Paths } from '@/data/paths';
 
-type HomeProps = {
-  writings: {
-    content: string;
-    data: {
-      [key: string]: any;
-    };
-    filePath: string;
-  }[];
-};
-
-const Home: React.FC<HomeProps> = ({ writings }) => {
+const Home: React.FC = () => {
   const title = 'Bailey Jennings - Home';
   const SEO = {
     title,
@@ -36,40 +17,9 @@ const Home: React.FC<HomeProps> = ({ writings }) => {
       <NextSeo {...SEO} />
       <div className={styles.container}>
         <h1>Home</h1>
-        <ul>
-          {writings.map((writing) => (
-            <li key={writing.filePath}>
-              <CustomLink
-                as={`${Paths.writings}/${writing.filePath.replace(
-                  /\.mdx?$/,
-                  '',
-                )}`}
-                href={`${Paths.writings}/[slug]`}>
-                {writing.data.title}
-              </CustomLink>
-            </li>
-          ))}
-        </ul>
       </div>
     </>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  // Creating an array of writings from `writings/` directory
-  const writings = writingsFilePaths.map((filePath) => {
-    const source = fs.readFileSync(path.join(WRITINGS_PATH, filePath));
-
-    const { content, data } = matter(source);
-
-    return {
-      content,
-      data,
-      filePath,
-    };
-  });
-
-  return { props: { writings } };
 };
 
 (Home as PageWithLayoutType).getLayout = (page) => {
--- src/pages/writings/index.tsx
+++ src/pages/writings/index.tsx
@@ -1,8 +1,24 @@
+import fs from 'fs';
 import { PageWithLayoutType } from '@/components/layouts/layouts.model';
 import { MainLayout } from '@/components/layouts/MainLayout/MainLayout';
+import { writingsFilePaths, WRITINGS_PATH } from '@/utils/mdxUtils';
+import { GetStaticProps } from 'next';
 import { NextSeo } from 'next-seo';
+import path from 'path';
+import matter from 'gray-matter';
+import { Paths } from '@/data/paths';
+import { CustomLink } from '@/components/CustomLink/CustomLink';
 
-const Writings: React.FC = () => {
+type WritingsProps = {
+  writings: {
+    content: string;
+    data: {
+      [key: string]: any;
+    };
+    filePath: string;
+  }[];
+};
+const Writings: React.FC<WritingsProps> = ({ writings }) => {
   const title = 'Bailey Jennings - Writings';
   const SEO = {
     title,
@@ -14,8 +30,40 @@ const Writings: React.FC = () => {
     <>
       <NextSeo {...SEO} />
       <h1>Writings</h1>
+      <ul>
+        {writings.map((writing) => (
+          <li key={writing.filePath}>
+            <CustomLink
+              as={`${Paths.writings}/${writing.filePath.replace(
+                /\.mdx?$/,
+                '',
+              )}`}
+              href={`${Paths.writings}/[slug]`}>
+              {writing.data.title}
+            </CustomLink>
+          </li>
+        ))}
+      </ul>
     </>
   );
+};
+
+// TODO: Create a HOF that grabs this data
+export const getStaticProps: GetStaticProps = async () => {
+  // Creating an array of writings from `writings/` directory
+  const writings = writingsFilePaths.map((filePath) => {
+    const source = fs.readFileSync(path.join(WRITINGS_PATH, filePath));
+
+    const { content, data } = matter(source);
+
+    return {
+      content,
+      data,
+      filePath,
+    };
+  });
+
+  return { props: { writings } };
 };
 
 (Writings as PageWithLayoutType).getLayout = (page) => {
